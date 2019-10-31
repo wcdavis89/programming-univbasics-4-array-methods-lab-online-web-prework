380bc74b8280f6230ff2753032f00bb82afd637a
Whittney Davis <wcdavis89@yahoo.com>
2019-10-31 14:58:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,6 @@
 def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.include?("element1")
-array.include?("element2")
+array.include?(element)
 end
 
 def using_sort(array)
